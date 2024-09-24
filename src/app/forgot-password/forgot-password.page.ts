0a95ea6384103c217526b5166168433f75f25755
BlaTV29 <164098430+BlaTV29@users.noreply.github.com>
2024-09-24 18:17:22 -0300

    App resgistro duoc1.0
    Se agregan, inicio de sesion, botones y mas interaciones

---
+++ src/app/forgot-password/forgot-password.page.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.page.html',
+  styleUrls: ['./forgot-password.page.scss'],
+})
+export class ForgotPasswordPage {
+  email:string='';
+
+  constructor(private router: Router, private alertController: AlertController) { }
+
+  async recuperarContrasena() {
+    if (!this.email) {
+      await this.mostrarMensaje('Por favor ingresa tu correo electrónico.');
+    
+    } else if (!this.validarEmail(this.email)) {
+      await this.mostrarMensaje('Por favor ingresa un correo electrónico válido.');
+    
+    } else {
+      await this.mostrarMensaje('Se ha enviado un enlace de recuperación a ' + this.email);
+    }
+
+    this.email = '';
+  }
+
+  validarEmail(email: string): boolean{
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  async mostrarMensaje(mensaje: string){
+    const alert = await this.alertController.create({
+      header: 'Recuperacion de Contraseña',
+      message: mensaje,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+}
+
+  
